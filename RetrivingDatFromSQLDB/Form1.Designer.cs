7d40fb612f3785fdb2a5992b12f05ca244fded25
Pasan Anuththara <pasananuththara19@gmail.com>
2020-07-21 09:44:38 +0530

    Add project files.

---
+++ RetrivingDatFromSQLDB/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace RetrivingDatFromSQLDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_firstName = new System.Windows.Forms.TextBox();
+            this.tb_lastName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_userName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_age = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_signup = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // tb_firstName
+            // 
+            this.tb_firstName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tb_firstName.Location = new System.Drawing.Point(137, 94);
+            this.tb_firstName.Name = "tb_firstName";
+            this.tb_firstName.Size = new System.Drawing.Size(165, 23);
+            this.tb_firstName.TabIndex = 1;
+            // 
+            // tb_lastName
+            // 
+            this.tb_lastName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tb_lastName.Location = new System.Drawing.Point(137, 132);
+            this.tb_lastName.Name = "tb_lastName";
+            this.tb_lastName.Size = new System.Drawing.Size(165, 23);
+            this.tb_lastName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Last Name";
+            // 
+            // tb_userName
+            // 
+            this.tb_userName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tb_userName.Location = new System.Drawing.Point(137, 170);
+            this.tb_userName.Name = "tb_userName";
+            this.tb_userName.Size = new System.Drawing.Size(165, 23);
+            this.tb_userName.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "User Name";
+            // 
+            // tb_password
+            // 
+            this.tb_password.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tb_password.Location = new System.Drawing.Point(137, 208);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.PasswordChar = '⁐';
+            this.tb_password.Size = new System.Drawing.Size(165, 23);
+            this.tb_password.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Password";
+            // 
+            // tb_age
+            // 
+            this.tb_age.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tb_age.Location = new System.Drawing.Point(137, 246);
+            this.tb_age.Name = "tb_age";
+            this.tb_age.Size = new System.Drawing.Size(165, 23);
+            this.tb_age.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 249);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Age";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(97, 20);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(153, 44);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Sign Up";
+            // 
+            // btn_signup
+            // 
+            this.btn_signup.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_signup.BackColor = System.Drawing.Color.YellowGreen;
+            this.btn_signup.FlatAppearance.BorderSize = 0;
+            this.btn_signup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_signup.Location = new System.Drawing.Point(215, 301);
+            this.btn_signup.Name = "btn_signup";
+            this.btn_signup.Size = new System.Drawing.Size(87, 27);
+            this.btn_signup.TabIndex = 11;
+            this.btn_signup.Text = "Sign Up";
+            this.btn_signup.UseVisualStyleBackColor = false;
+            this.btn_signup.Click += new System.EventHandler(this.btn_signup_Click);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_cancel.BackColor = System.Drawing.Color.Coral;
+            this.btn_cancel.FlatAppearance.BorderSize = 0;
+            this.btn_cancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_cancel.Location = new System.Drawing.Point(122, 301);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(87, 27);
+            this.btn_cancel.TabIndex = 12;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(349, 362);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_signup);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tb_age);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tb_password);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tb_userName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb_lastName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_firstName);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_firstName;
+        private System.Windows.Forms.TextBox tb_lastName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_userName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_age;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btn_signup;
+        private System.Windows.Forms.Button btn_cancel;
+    }
+}
+
---
+++ RetrivingDatFromSQLDB/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+
+namespace RetrivingDatFromSQLDB
+{
+    public partial class Form1 : Form
+    {
+        String 
+            FirstName,
+            LastName,
+            UserName,
+            Password;
+
+        int Age;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_signup_Click(object sender, EventArgs e)
+        {
+            FirstName = tb_firstName.Text;
+            LastName = tb_lastName.Text;
+            UserName = tb_userName.Text;
+            Password = tb_password.Text;
+            Age = int.Parse(tb_age.Text);
+
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Databases\UserData7_21.mdf;Integrated Security=True;Connect Timeout=30");
+            String qry = "INSERT INTO user VALUES('" + FirstName + "', '" + LastName + "', '" + UserName + "', '" + Password + "', '" + Age + "')";
+            SqlCommand cmd = new SqlCommand(qry, con);
+
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("✔ SignUp Successfull", "Successfull");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            finally
+            {
+                con.Close();
+            }
+           
+        }
+    }
+}
